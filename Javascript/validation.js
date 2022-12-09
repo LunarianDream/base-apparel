fb566854125df0cf25dbb69c364fa8ab56f48abd
LunarianDream <jelissasummers@aol.com>
2022-12-08 19:17:42 -0500

    Validation Progress
    jQuery validation for blank input complete

---
+++ Javascript/validation.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    let isValid = true;
+    $('.error').hide();
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+    });
+
+    let mailingValidation = function () {
+        if ($('#email').val() == '') {
+            $('#email').css('border-color', 'var(--softRed)');
+            $('.error').show();
+            $('.error').text(`Please provide a valid email`);
+            isValid = false;
+        } else {
+            isValid = true;
+            $('#email').css('border-color', 'gainsboro');
+            $('.error').hide();
+
+        }
+    };
+
+    $('.form_submit').click(mailingValidation);
+});
